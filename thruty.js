3eab47c084481433d6dc8096d9dfaccd0034f194
Danilo <danilocunhatj@gmail.com>
2022-06-23 17:03:36 -0300

    Segundo caso

---
+++ thruty.js
@@ -0,0 +1,17 @@
+const usuario = true;
+const contapaga = false;
+
+console.log (0 == false);
+console.log ("" == false);
+console.log (1 == true);
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+console.log (minhaVar);
+console.log (varNull);
+
+console.log (typeof minhaVar);
+console.log (typeof varNull);
