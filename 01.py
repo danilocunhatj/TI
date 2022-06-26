73396aaa149e54680630cb3aebaf31183760b69f
Danilo <danilocunhatj@gmail.com>
2022-06-25 21:08:46 -0300

    pyton aula 1

---
+++ 01.py
@@ -0,0 +1,8 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute = input("Digite o seu número: ")
+print("Você digitou: ", chute)
